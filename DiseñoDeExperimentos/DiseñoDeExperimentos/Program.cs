9d81ed1f06041126030d72bbc6e8655b312958c6
manuelq12 <manuelq12@users.noreply.github.com>
2018-09-08 18:10:14 -0500

    QuickSort

--- DiseñoDeExperimentos/DiseñoDeExperimentos/Program.cs
+++ DiseñoDeExperimentos/DiseñoDeExperimentos/Program.cs
@@ -10,6 +10,36 @@ namespace DiseñoDeExperimentos
     {
         static void Main(string[] args)
         {
+
+        }
+
+
+        public static void QuickSort(int[] A, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = Partition(A, p, r);
+                QuickSort(A, p, q - 1);
+                QuickSort(A, q + 1, r);
+            }
+
+
+        }
+
+        public static int Partition(int[] A, int p, int r)
+        {
+            int X = A[r];
+            int i = p - 1;
+            for (int j = p; j < r - 1; j++)
+            {
+                if (A[j] <= X)
+                {
+                    i = i + 1;
+                    A[i] = A[j];
+                }
+                A[i + 1] = A[r];
+            }
+            return i + 1;
         }
     }
 }
